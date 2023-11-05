784a8b592f050677d953ffe30a17c778db311844
vpraharsha03 <vpr03>
2023-11-05 11:17:49 +0530

    Update rootfs version to 20231029T164701Z

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export PATH=/usr/.bin:$PATH
 
 # Env variables for the Gentoo image
 OS_VER="stable"
-ROOTFS_VER="20230827T170145Z"
+ROOTFS_VER="20231029T164701Z"
 ROOTFS_URL="https://gentoo.osuosl.org/releases/amd64/autobuilds/current-stage3-amd64-nomultilib-openrc/stage3-amd64-nomultilib-openrc-${ROOTFS_VER}.tar.xz"
 
 # Environment variables for Yuk7's wsldl
