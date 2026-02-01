629dfa5d0e03a92e89ff71014fc578de9b9dd2f2
BinaryEndeavours <vpraharsha3@gmail.com>
2026-02-01 00:08:05 +0000

    scrape_latest_version: updated env.sh [ci skip]

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 # Env variables for the Gentoo image
 OS_VER="stable"
-ROOTFS_VER=20260121T111559Z
+ROOTFS_VER=20260125T170113Z
 ROOTFS_URL="https://gentoo.osuosl.org/releases/amd64/autobuilds/current-stage3-amd64-nomultilib-openrc/stage3-amd64-nomultilib-openrc-${ROOTFS_VER}.tar.xz"
 
 # Environment variables for Yuk7's wsldl
